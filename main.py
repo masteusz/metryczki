10449ee9a35b24abf41412d8cf0280569274f608
Mateusz Stankiewicz <mateusz@mateusz.at>
2021-12-11 15:30:47 +0100

    Init

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from pylatex import Document, Section, Subsection, Tabular
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+if __name__ == "__main__":
+    geometry_options = {"documentclass": "a4paper", "margin": "0.5in"}
+    doc = Document(geometry_options=geometry_options)
 
+    with doc.create(Section("The simple stuff")):
+        with doc.create(Subsection("Table of something")):
+            with doc.create(Tabular("rc|cl")) as table:
+                table.add_hline()
+                table.add_row((1, 2, 3, 4))
+                table.add_hline(1, 2)
+                table.add_empty_row()
+                table.add_row((4, 5, 6, 7))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    doc.generate_pdf("full", clean_tex=False)
